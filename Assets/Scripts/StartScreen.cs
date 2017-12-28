7970aed2bf71e850678ab88b1da210203d70ba3c
kikemunozj <kikemunozj@gmail.com>
2017-12-28 19:33:12 +0000

    FirstCommit

---
+++ Assets/Scripts/StartScreen.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartScreen : MonoBehaviour {
+
+    float levelLoadDelay = 2f;
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+    void Start()
+    {
+        Invoke("LoadFirstScene", levelLoadDelay);
+    }
+
+    void LoadFirstScene()
+
+    {
+        SceneManager.LoadScene(1);
+    }
+}
