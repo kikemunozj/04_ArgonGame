7abdef055cfb2a0c852d36513aded0b3e7356452
kikemunozj <kikemunozj@gmail.com>
2017-12-28 23:58:10 +0000

    Control added

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,19 +1,53 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityStandardAssets.CrossPlatformInput;
 
+public class Player : MonoBehaviour {
 
+    [Tooltip("In m/s")][SerializeField] float speed = 10f;
+    [SerializeField] float clampFactor = 4f;
+    [SerializeField] float pitchFactor = -5f;
+    [SerializeField] float yawFactor = 5f;
+    [SerializeField] float positionPitchFactor = 1f;
+    [SerializeField] float controlPitchFactor = 5f;
 
-public class player : MonoBehaviour {
+    float xThrow;
+    float yThrow;
 
-	// Use this for initialization
 	void Start () {
 		
 	}
 	
-	// Update is called once per frame
-	void Update () {
-        float horizontalThrow = CrossPlatformInputManager.GetAxis("Horizontal");
-	}
+	
+	void Update ()
+    {
+        ProcessTranslation();
+        ProcessRotation();
+    }
+
+    private void ProcessRotation()
+    {
+        float pitch = transform.localPosition.y * pitchFactor + yThrow;
+        float yaw = transform.localPosition.x * yawFactor;
+        float roll = transform.localPosition.x * positionPitchFactor;
+
+        transform.localRotation = Quaternion.Euler(pitch , yaw, roll);
+    }
+
+    private void ProcessTranslation()
+    {
+        xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        float xOffset = xThrow * speed * Time.deltaTime;
+        float rawNewXPos = transform.localPosition.x + xOffset;
+        float newXPos = Mathf.Clamp(rawNewXPos, -clampFactor, clampFactor);
+
+        yThrow = CrossPlatformInputManager.GetAxis("Vertical");
+        float yOffset = -yThrow * speed * Time.deltaTime;
+        float rawNewYPos = transform.localPosition.y + yOffset;
+        float newYPos = Mathf.Clamp(rawNewYPos, -clampFactor * 0.6f, clampFactor * 0.6f);
+
+        transform.localPosition = new Vector3(newXPos, newYPos, transform.localPosition.z);
+    }
 }
